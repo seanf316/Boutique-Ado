4c47468a164b9aea4a88b5e8b4678b623666326d
seanf316 <seanfinn316@gmail.com>
2023-04-25 23:04:07 +0000

    Stripe setup

--- checkout/views.py
+++ checkout/views.py
@@ -12,6 +12,10 @@ def checkout(request):
 
     order_form = OrderForm()
     template = "checkout/checkout.html"
-    context = {"order_form": order_form}
+    context = {
+        "order_form": order_form,
+        "stripe_public_key": "pk_test_51N0ucVJ2Zqv7CR6ur2j8RGhR537KAAMs1CCbmLroY87VpORKCunhsKdKgTUBOzQibOlfhTUJPwpZIwxUD0nGPHOw00iZD6JZTk",
+        "client_secret": "test client secret",
+    }
 
     return render(request, template, context)
