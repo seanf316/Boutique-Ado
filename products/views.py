4015f7e6ef57667feb8c1536dfd4b17c650acf79
seanf316 <seanfinn316@gmail.com>
2023-05-01 13:17:49 +0000

    Product Admin - Add product view and template

--- products/views.py
+++ products/views.py
@@ -3,6 +3,7 @@ from django.contrib import messages
 from django.db.models import Q
 from django.db.models.functions import Lower
 from .models import Product, Category
+from .forms import ProductForm
 
 # Create your views here.
 
@@ -17,20 +18,20 @@ def all_products(request):
     direction = None
 
     if request.GET:
-        if 'sort' in request.GET:
-            sortkey = request.GET['sort']
+        if "sort" in request.GET:
+            sortkey = request.GET["sort"]
             sort = sortkey
-            if sortkey == 'name':
-                sortkey = 'lower_name'
-                products = products.annotate(lower_name=Lower('name'))
-            if sortkey == 'category':
-                sortkey = 'category__name'
-            if 'direction' in request.GET:
-                direction = request.GET['direction']
-                if direction == 'desc':
-                    sortkey = f'-{sortkey}'
+            if sortkey == "name":
+                sortkey = "lower_name"
+                products = products.annotate(lower_name=Lower("name"))
+            if sortkey == "category":
+                sortkey = "category__name"
+            if "direction" in request.GET:
+                direction = request.GET["direction"]
+                if direction == "desc":
+                    sortkey = f"-{sortkey}"
             products = products.order_by(sortkey)
-            
+
         if "category" in request.GET:
             categories = request.GET["category"].split(",")
             products = products.filter(category__name__in=categories)
@@ -49,13 +50,13 @@ def all_products(request):
             )
             products = products.filter(queries)
 
-    current_sorting = f'{sort}_{direction}'
+    current_sorting = f"{sort}_{direction}"
 
     context = {
         "products": products,
         "search_term": query,
         "current_categories": categories,
-        'current_sorting': current_sorting,
+        "current_sorting": current_sorting,
     }
 
     return render(request, "products/products.html", context)
@@ -71,3 +72,14 @@ def product_detail(request, product_id):
     }
 
     return render(request, "products/product_detail.html", context)
+
+
+def add_product(request):
+    """Add a product to the store"""
+    form = ProductForm()
+    template = "products/add_product.html"
+    context = {
+        "form": form,
+    }
+
+    return render(request, template, context)
